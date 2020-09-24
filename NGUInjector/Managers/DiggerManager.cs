69784090d5f5ef48a0aa380caef54c97de2ed26a
Rohan Vazarkar <rvazarkr@gmail.com>
2020-09-24 10:52:33 -0400

    Remove tooltips from capSavedDiggers to prevent spam

--- NGUInjector/Managers/DiggerManager.cs
+++ NGUInjector/Managers/DiggerManager.cs
@@ -88,8 +88,48 @@ namespace NGUInjector
             {
                 if (Main.Character.diggers.diggers[i].active)
                 {
-                    Main.Character.allDiggers.setLevelMaxAffordable(i);
+                    SetLevelMaxAffordable(i);
                 }
+            }
+        }
+
+        private static void SetLevelMaxAffordable(int id)
+        {
+            if (id < 0 || id > Main.Character.diggers.diggers.Count)
+                return;
+            var curLevel = Main.Character.diggers.diggers[id].curLevel;
+            Main.Character.diggers.diggers[id].curLevel = 0L;
+            if (Main.Character.goldPerSecond() < Main.Character.allDiggers.drain(id, 1, true))
+            {
+                Main.Character.diggers.diggers[id].curLevel = curLevel;
+            }
+            else
+            {
+                var num1 = Main.Character.goldPerSecond();
+                var num2 = Main.Character.allDiggers.baseGPSDrain[id];
+                var a = Main.Character.allDiggers.gpsGrowthRate[id];
+                var num3 = Math.Min((long)(Math.Log(num1 / num2, Math.E) / Math.Log(a, Math.E)), Main.Character.diggers.diggers[id].maxLevel);
+                if (num3 < 0L)
+                    num3 = 0L;
+                if (num3 > Main.Character.diggers.diggers[id].maxLevel)
+                    num3 = Main.Character.diggers.diggers[id].maxLevel;
+                Main.Character.diggers.diggers[id].curLevel = num3;
+                if (Main.Character.diggers.diggers[id].curLevel == 0L && Main.Character.diggers.diggers[id].active)
+                {
+                    var num4 = 0;
+                    while (num4 < Main.Character.diggers.activeDiggers.Count)
+                        ++num4;
+                    Main.Character.diggers.diggers[id].active = false;
+                    Main.Character.diggers.activeDiggers.RemoveAt(Main.Character.diggers.activeDiggers.IndexOf(id));
+                }
+                if (Main.Character.grossGoldPerSecond() < Main.Character.allDiggers.totalGPSDrain())
+                {
+                    Main.Character.diggers.diggers[id].curLevel = curLevel;
+                }
+                else if (!Main.Character.diggers.diggers[id].active && Main.Character.diggers.diggers[id].curLevel > 0L && Main.Character.diggers.activeDiggers.Count < Main.Character.allDiggers.maxDiggerSlots())
+                    Main.Character.allDiggers.activateDigger(id);
+
+                Main.Character.allDiggers.refreshMenu();
             }
         }
 
