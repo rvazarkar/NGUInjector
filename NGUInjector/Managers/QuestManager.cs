34573543ca2d4180a1df2bc4273598b2f73feec2
Rohan Vazarkar <rvazarkr@gmail.com>
2020-10-02 21:29:54 -0400

    Add logging for questing

--- NGUInjector/Managers/QuestManager.cs
+++ NGUInjector/Managers/QuestManager.cs
@@ -15,15 +15,18 @@ namespace NGUInjector.Managers
         {
             if (_character.beastQuestController.readyToHandIn())
             {
+                Log("Turning in quest");
                 if (!_character.beastQuest.usedButter)
                 {
                     if (_character.beastQuest.reducedRewards && Settings.UseButterMinor)
                     {
+                        Log("Buttering Minor Quest");
                         _character.beastQuestController.tryUseButter();
                     }
 
                     if (!_character.beastQuest.reducedRewards && Settings.UseButterMajor)
                     {
+                        Log("Buttering Major Quest");
                         _character.beastQuestController.tryUseButter();
                     }
                 }
@@ -105,91 +108,6 @@ namespace NGUInjector.Managers
                 }
 
                 SetIdleMode(!Settings.ManualMinors);
-            }
-
-        }
-
-        internal void ManageQuestsOld()
-        {
-            //We're in a quest already.
-            if (_character.beastQuest.inQuest)
-            {
-                // Its a minor quest
-                if (_character.beastQuest.reducedRewards)
-                {
-                    if (Settings.ManualMinors)
-                    {
-                        if (_character.beastQuest.idleMode)
-                        {
-                            _character.beastQuest.idleMode = false;
-                            _character.beastQuestController.updateButtons();
-                            _character.beastQuestController.updateButtonText();
-                        }
-                    }
-                    else
-                    {
-                        if (!_character.beastQuest.idleMode)
-                        {
-                            _character.beastQuest.idleMode = true;
-                            _character.beastQuestController.updateButtons();
-                            _character.beastQuestController.updateButtonText();
-                        }
-                    }
-
-                    //Check if we want to abandon minor quests in favor of major quests
-                    if (Settings.AllowMajorQuests && Settings.AbandonMinors & _character.beastQuest.curBankedQuests > 0)
-                    {
-                        //Check if we're under the threshold for abandoning
-                        if ((_character.beastQuest.curDrops / _character.beastQuest.targetDrops) * 100 <
-                            Settings.MinorAbandonThreshold)
-                        {
-                            //If all this is true get rid of this minor quest and pick up a new one.
-                            _character.beastQuestController.skipQuest();
-                            _character.settings.useMajorQuests = true;
-                            _character.beastQuestController.updateButtons();
-                            _character.beastQuestController.updateButtonText();
-                            _character.beastQuestController.startQuest();
-
-                            //Combat logic will pick up from here
-                            return;
-                        }
-                    }
-
-                }
-
-                //We have nothing else to do here
-                return;
-            }
-
-            //We're not in a quest, so we need to accept a new one
-            if (Settings.AllowMajorQuests && _character.beastQuest.curBankedQuests > 0)
-            {
-                //Turn off idle mode
-                _character.beastQuest.idleMode = false;
-                _character.beastQuestController.updateButtons();
-                _character.beastQuestController.updateButtonText();
-                //We're allowed to accept major quests, so toggle this to true, and then lets accept one.
-                _character.settings.useMajorQuests = true;
-                _character.beastQuestController.updateButtons();
-                _character.beastQuestController.updateButtonText();
-                _character.beastQuestController.startQuest();
-            }
-            else
-            {
-                if (Settings.ManualMinors)
-                {
-                    _character.beastQuest.idleMode = false;
-                    _character.beastQuestController.updateButtons();
-                    _character.beastQuestController.updateButtonText();
-                }
-                else
-                {
-                    _character.beastQuest.idleMode = true;
-                    _character.beastQuestController.updateButtons();
-                    _character.beastQuestController.updateButtonText();
-                }
-                _character.settings.useMajorQuests = false;
-                _character.beastQuestController.startQuest();
             }
         }
     }
