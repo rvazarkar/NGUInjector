145f54fe29d6126ea4290f8f219f3a62d76d1e0c
Altizar <altizar_23@hotmail.com>
2020-10-04 07:27:40 -0700

    Remove excess looping, clean up logic

--- NGUInjector/Managers/WishManager.cs
+++ NGUInjector/Managers/WishManager.cs
@@ -27,43 +27,38 @@ namespace NGUInjector.Managers
 
         public void BuildWishList()
         {
-            _curValidUpgradesList.Clear();
-            var _wishPriorities = new List<int>();
+            var dictDouble = new Dictionary<int, double>();
+            var wishPriorities = new List<int>();
+            
             for (var i = 0; i < Settings.WishPriorities.Count(); i++)
             {
                 if (isValidWish(Settings.WishPriorities[i]))
                 {
-                    _wishPriorities.Add(Settings.WishPriorities[i]);
+                    wishPriorities.Add(Settings.WishPriorities[i]);
                 }
             }
+
             for (var i = 0; i < _character.wishes.wishes.Count; i++)
             {
-                if (_wishPriorities.Contains(i))
+                if (wishPriorities.Contains(i))
                 {
                     continue;
                 }
                 if (isValidWish(i))
                 {
-                    _curValidUpgradesList.Add(i);
+                    dictDouble.Add(i, _character.wishesController.properties[i].wishSpeedDivider);
                 }
             }            
-
-            var dictDouble = new Dictionary<int, double>();
-
-            for (var i = 0; i < _curValidUpgradesList.Count; i++)
-            {
-                dictDouble.Add(_curValidUpgradesList[i], _character.wishesController.properties[_curValidUpgradesList[i]].wishSpeedDivider);
-            }
-
             dictDouble = (from x in dictDouble
                                orderby x.Value
                                select x).ToDictionary(x => x.Key, x => x.Value);
+
             _curValidUpgradesList.Clear();
-            for (var i = 0; i < _wishPriorities.Count; i++)
+            for (var i = 0; i < wishPriorities.Count; i++)
             {
-                _curValidUpgradesList.Add(_wishPriorities[i]);
+                _curValidUpgradesList.Add(wishPriorities[i]);
             }
-                for (var j = 0; j < dictDouble.Count; j++)
+            for (var j = 0; j < dictDouble.Count; j++)
             {
                 _curValidUpgradesList.Add(dictDouble.ElementAt(j).Key);
             }
