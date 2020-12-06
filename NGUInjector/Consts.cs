ea3fe9a93cff31f8ae01d81d498d12833a6054db
Rohan Vazarkar <rvazarkr@gmail.com>
2020-12-06 14:19:55 -0500

    Update gear const to the right value

--- NGUInjector/Consts.cs
+++ NGUInjector/Consts.cs
@@ -8,6 +8,6 @@ namespace NGUInjector
     internal static class Consts
     {
         internal const int MAX_WISH_ID = 230;
-        internal const int MAX_GEAR_ID = 214;
+        internal const int MAX_GEAR_ID = 414;
     }
 }
