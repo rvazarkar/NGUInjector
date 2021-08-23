b6fd6d4d5a3528878cd618dbf6948ac3455e93ff
Altizar <altizar_23@hotmail.com>
2021-08-22 18:23:55 -0700

    Bestaug now uses standard aug logic for cap/bbing, fixed logic for BBing, now seems to be more accurate to BB/CAP

--- NGUInjector/AllocationProfiles/BreakpointTypes/BestAug.cs
+++ NGUInjector/AllocationProfiles/BreakpointTypes/BestAug.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace NGUInjector.AllocationProfiles.BreakpointTypes
 {
-    internal class BestAug : BaseBreakpoint
+    internal class BestAug : AugmentBP
     {
         internal int RebirthTime { get; set; }
         private bool _useUpgrades;
@@ -120,123 +120,6 @@ namespace NGUInjector.AllocationProfiles.BreakpointTypes
             }
             return (double)aug.baseBoost * aug.getUpgradeBoost() * Math.Pow(Character.augments.augs[aug.id].augLevel + 500f, aug.augTierBonus()) - aug.baseBoost * aug.getUpgradeBoost() * Math.Pow(Character.augments.augs[aug.id].augLevel + 1f, (float)aug.augTierBonus());
         }
-
-        protected override bool CorrectResourceType()
-        {
-            return Type == ResourceType.Energy;
-        }
-
-        internal float CalculateAugCap(int index, float allocation)
-        {
-            var calcA = CalculateAugCapCalc(500, index, allocation);
-            if (calcA.PPT < 1)
-            {
-                var calcB = CalculateAugCapCalc(calcA.GetOffset(), index, allocation);
-                return calcB.Num;
-            }
-
-            return calcA.Num;
-        }
-
-        internal CapCalc CalculateAugCapCalc(int offset, int index, float allocation)
-        {
-            int augIndex;
-            var ret = new CapCalc
-            {
-                Num = 0,
-                PPT = 1
-            };
-            double formula = 0;
-            if (index % 2 == 0)
-            {
-                augIndex = index / 2;
-                formula = 50000 * (1f + Character.augments.augs[augIndex].augLevel + offset) /
-                    (Character.totalEnergyPower() *
-                    (1 + Character.inventoryController.bonuses[specType.Augs]) *
-                    Character.inventory.macguffinBonuses[12] *
-                    Character.hacksController.totalAugSpeedBonus() *
-                    Character.cardsController.getBonus(cardBonus.augSpeed) *
-                    Character.adventureController.itopod.totalAugSpeedBonus() *
-                    (1.0 + Character.allChallenges.noAugsChallenge.evilCompletions() * 0.0500000007450581));
-
-                if (Character.allChallenges.noAugsChallenge.completions() >= 1)
-                {
-                    formula /= 1.10000002384186;
-                }
-                if (Character.allChallenges.noAugsChallenge.evilCompletions() >= Character.allChallenges.noAugsChallenge.maxCompletions)
-                {
-                    formula /= 1.25;
-                }
-                if (Character.settings.rebirthDifficulty >= difficulty.sadistic)
-                {
-                    formula *= Character.augmentsController.augments[augIndex].sadisticDivider();
-                }
-                if (Character.settings.rebirthDifficulty == difficulty.normal)
-                {
-                    formula *= Character.augmentsController.normalAugSpeedDividers[augIndex];
-                }
-                else if (Character.settings.rebirthDifficulty == difficulty.evil)
-                {
-                    formula *= Character.augmentsController.evilAugSpeedDividers[augIndex];
-                }
-                else if (Character.settings.rebirthDifficulty == difficulty.sadistic)
-                {
-                    formula *= Character.augmentsController.sadisticAugSpeedDividers[augIndex];
-                }
-            }
-            else
-            {
-                augIndex = (index - 1) / 2;
-                formula = 50000 * (1f + Character.augments.augs[augIndex].upgradeLevel + offset) /
-                    (Character.totalEnergyPower() *
-                    (1 + Character.inventoryController.bonuses[specType.Augs]) *
-                    Character.inventory.macguffinBonuses[12] *
-                    Character.hacksController.totalAugSpeedBonus() *
-                    Character.cardsController.getBonus(cardBonus.augSpeed) *
-                    Character.adventureController.itopod.totalAugSpeedBonus() *
-                    (1.0 + Character.allChallenges.noAugsChallenge.evilCompletions() * 0.0500000007450581));
-
-                if (Character.allChallenges.noAugsChallenge.completions() >= 1)
-                {
-                    formula /= 1.10000002384186;
-                }
-                if (Character.allChallenges.noAugsChallenge.evilCompletions() >= Character.allChallenges.noAugsChallenge.maxCompletions)
-                {
-                    formula /= 1.25;
-                }
-                if (Character.settings.rebirthDifficulty >= difficulty.sadistic)
-                {
-                    formula *= Character.augmentsController.augments[augIndex].sadisticDivider();
-                }
-                if (Character.settings.rebirthDifficulty == difficulty.normal)
-                {
-                    formula *= Character.augmentsController.normalUpgradeSpeedDividers[augIndex];
-
-                }
-                else if (Character.settings.rebirthDifficulty == difficulty.evil)
-                {
-                    formula *= Character.augmentsController.evilUpgradeSpeedDividers[augIndex];
-
-                }
-                else if (Character.settings.rebirthDifficulty == difficulty.sadistic)
-                {
-                    formula *= Character.augmentsController.sadisticUpgradeSpeedDividers[augIndex];
-                }
-            }
-            if (formula >= Character.hardCap())
-                formula = Character.hardCap();
-            var num4 = formula <= 1.0 ? 1L : (long)formula;
-            var num = (long)(num4 / (long)Math.Ceiling(num4 / (double)allocation) * 1.00000202655792);
-            if (num + 1L <= long.MaxValue)
-                ++num;
-            if (num > Character.idleEnergy)
-                num = Character.idleEnergy;
-            if (num < 0L)
-                num = 0L;
-            var ppt = (double)num / num4;
-            ret.Num = num;
-            ret.PPT = ppt;
-            return ret;
-        }
+                        
     }
 }
